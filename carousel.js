c3cdf07e7b0660fbf6cb3df619afaf545a3d77c1
kliuchyk <kliuchyk@gmail.com>
2019-12-09 12:31:24 +0200

    add carousel

---
+++ carousel.js
@@ -0,0 +1,38 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("comments-group");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].className += " active";
+}
+
+const resizeContainer = () => {
+  const parent = document.querySelector(".section-comments__list");
+  const comments = document.querySelector(".comments-group");
+  const commentsHeight = comments.offsetHeight + "px";
+
+  parent.style.height = commentsHeight;
+};
+
+window.addEventListener("DOMContentLoaded", resizeContainer);
+window.addEventListener("resize", resizeContainer);
